5908f59d6dbfb48420c857f7118aa908b1462bf2
SACHINNXD <145748982+SACHINNXD@users.noreply.github.com>
2026-01-18 11:46:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    const nav = document.getElementById("sideNav");
+    const overlay = document.getElementById("overlay");
+
+    nav.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
